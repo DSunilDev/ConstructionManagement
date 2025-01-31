63ae6c4dc81df4ed694d06a4163cc98ab77285c5
DSunilDev <sunilkumardeveops@gmail.com>
2025-01-31 12:01:50 +0530

    Initialize

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app=express();
 
 app.use(express.urlencoded({extended:true}))
 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 
+//Home Page Route
 
-
-
-
+app.get('/',function(req,res)
+{
+    res.render('index')
+})
 
 
 

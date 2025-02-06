1728d59f194fc96f7cdec3d7f030f8fd214a45fe
DSunilDev <sunilkumardeveops@gmail.com>
2025-02-06 09:28:05 +0530

    Initial Pages Creation

--- app.js
+++ app.js
@@ -10,15 +10,31 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-//Home Page Route
+//Home Page Route - Acts as Dashboard of the Software 
 
 app.get('/',function(req,res)
 {
     res.render('index')
 })
 
+//Users Signup and Login Route
+
+app.get('/signup',function(req,res)
+{
+    res.render('signup')
+})
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
 
 
+//About the Software Page
+
+app.get('/about',function(req,res)
+{
+    res.render('about')
+})
 
 
 app.use(function(req,res)
